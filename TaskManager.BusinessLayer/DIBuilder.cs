721cd2bb7ccaa186d43028f081f08a674e33e332
reachsanthosh <reachsanthosh0305@gmail.com>
2018-12-14 09:24:45 +0530

    Commit changes

---
+++ TaskManager.BusinessLayer/DIBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager.DataAccessLayer;
+
+namespace TaskManager.BusinessLayer
+{
+    public static class DIBuilder
+    {
+        public static void Build(IServiceCollection services, IConfiguration config)
+        {
+            services.AddTransient<ITaskRepository, TaskRepository>();         
+            services.AddEntityFrameworkSqlServer().
+                AddDbContext<TaskDbContext>(option => option.UseSqlServer(config.GetSection("Database").GetSection("Connection").Value));
+
+        }
+    }
+}
---
+++ TaskManager.DataAccessLayer/TaskRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Model;
+
+namespace TaskManager.DataAccessLayer
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly TaskDbContext taskDbContext;
+        private readonly ILogger<TaskRepository> logger;
+        public TaskRepository(TaskDbContext taskDbContext, ILogger<TaskRepository> logger)
+        {
+            this.taskDbContext = taskDbContext;
+            this.logger = logger;
+        }
+        public async Task<int> DeleteAsync(TaskDetail entity)
+        {
+            taskDbContext.Tasks.Remove(entity);
+
+            return await taskDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TaskDetail>> GetAllAsync()
+        {
+            return await taskDbContext.Tasks.AsNoTracking<TaskDetail>().ToListAsync();
+        }
+
+        public async Task<TaskDetail> GetAsync(int id)
+        {
+            return await taskDbContext.Tasks.FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<int> InsertAsync(TaskDetail entity)
+        {
+            taskDbContext.Tasks.Add(entity);
+            return await taskDbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> UpdateAsync(int id,TaskDetail entity)
+        {           
+            taskDbContext.Tasks.Update(entity);
+            return await taskDbContext.SaveChangesAsync();
+        }
+    }
+}
