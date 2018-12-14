51f11d7d246fcf9ae4783656d02e37a9b59915fd
reachsanthosh <reachsanthosh0305@gmail.com>
2018-12-14 10:14:07 +0530

    Updating the Model

---
+++ TaskManager.Model/TaskDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TaskManager.Model
+{
+    public class TaskDetails
+    {
+        public int TaskId { get; set; }
+
+        public string TaskName { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int Priority { get; set; }
+
+       public bool EndTask { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
